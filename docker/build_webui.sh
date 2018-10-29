0f0b9f7f7f1bba89224d547d0de9bbdf9048e8e5
azban <me@azban.net>
2018-10-29 14:30:43 -0700

    ops: add docker registries, templatize k8s yaml

--- docker/build_webui.sh
+++ docker/build_webui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(cd ../ && tar c --exclude="web-ui/node_modules" web-ui docker | sudo docker build -t near/dash-web-ui -f docker/WebUI.dockerfile -)
+(cd ../ && tar c --exclude="web-ui/node_modules" web-ui docker | sudo docker build -t near/dash-webui -f docker/WebUI.dockerfile -)
