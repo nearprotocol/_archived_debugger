f510692560a6f33d48ad7449c130312e7775c62c
azban <me@azban.net>
2019-02-20 17:19:42 -0800

    move menu to separate file

---
+++ web-ui/src/Menu.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { Image, Menu } from 'semantic-ui-react'
+
+import LogoImage from './images/explorer-logo.svg';
+import HomeImage from './images/icon-home.svg';
+import TransactionsImage from './images/icon-transactions.svg'
+
+class MenuComponent extends React.Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    return (
+      <Menu borderless>
+        <Menu.Item header><Image src={LogoImage}/></Menu.Item>
+        <Menu.Item
+          name='beaconChain'
+          active={this.state.activeItem === 'beaconChain'}
+          onClick={this.handleItemClick}
+        >
+          <Image className="Menu-icon" src={HomeImage}/>
+          Beacon Chain  
+        </Menu.Item>
+        <Menu.Item
+          name='shardChains'
+          active={this.state.activeItem === 'shardChains'}
+          onClick={this.handleItemClick}
+        >
+          <Image className="Menu-icon" src={HomeImage}/>
+          Shard Chains  
+        </Menu.Item>
+        <Menu.Item
+          name='transactions'
+          active={this.state.activeItem === 'transactions'}
+          onClick={this.handleItemClick}
+        >
+          <Image className="Menu-icon" src={TransactionsImage}/>
+          Transactions
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
+
+export const MenuWithRouter = withRouter(MenuComponent)
--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -4,9 +4,7 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  withRouter,
 } from 'react-router-dom'
-import { Image, Menu, Header, Label } from 'semantic-ui-react'
 
 import './index.css'
 import {
@@ -15,68 +13,15 @@ import {
 } from './Errors';
 import * as serviceWorker from './serviceWorker';
 
-import Blocks from './images/icon-blocks.svg';
-import Logo from './images/explorer-logo.svg';
-import Home from './images/icon-home.svg';
-import TransactionsIcon from './images/icon-transactions.svg'
-
 import { BeaconBlockDetailWithRouter } from './BeaconBlockDetail';
 import { BeaconChainDetailWithRouter } from './BeaconChainDetail';
 import { ContractDetailWithRouter } from './ContractDetail';
 import { ShardBlockDetailWithRouter } from './ShardBlockDetail';
 import { ShardChainDetailWithRouter } from './ShardChainDetail';
 import { TransactionDetailWithRouter } from './TransactionDetail';
+import { MenuWithRouter } from './Menu';
 
 const PATH_PREFIX = process.env.REACT_APP_PATH_PREFIX
-
-class AppHeader extends React.Component {
-  state = {}
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  goBack = () => {
-    if (this.props.location.pathname === "/not_found") {
-      console.log("going")
-      this.props.history.go(-2)
-    } else {
-      this.props.history.goBack()
-    }
-  }
-
-  render() {
-    return (
-      <Menu borderless>
-        <Menu.Item header><Image src={Logo}/></Menu.Item>
-        <Menu.Item
-          name='beaconChain'
-          active={this.state.activeItem === 'beaconChain'}
-          onClick={this.handleItemClick}
-        >
-            <Image className="Menu-icon" src={Home}/>
-            Beacon Chain  
-        </Menu.Item>
-        <Menu.Item
-          name='shardChains'
-          active={this.state.activeItem === 'shardChains'}
-          onClick={this.handleItemClick}
-        >
-            <Image className="Menu-icon" src={Home}/>
-            Shard Chains  
-        </Menu.Item>
-        <Menu.Item
-          name='transactions'
-          active={this.state.activeItem === 'transactions'}
-          onClick={this.handleItemClick}
-        >
-            <Image className="Menu-icon" src={TransactionsIcon}/>
-            Transactions
-        </Menu.Item>
-      </Menu>
-    )
-  }
-}
-
-const AppHeaderWithRouter = withRouter(AppHeader)
 
 class Routing extends React.Component {
   render() {
@@ -84,7 +29,7 @@ class Routing extends React.Component {
       <div className="App">
         <Router basename={PATH_PREFIX}>
           <div>
-            <AppHeaderWithRouter />
+            <MenuWithRouter />
             <Switch>
               <Route exact path="/" component={BeaconChainDetailWithRouter} />
               <Route exact path="/beacon-chain" component={BeaconChainDetailWithRouter} />
