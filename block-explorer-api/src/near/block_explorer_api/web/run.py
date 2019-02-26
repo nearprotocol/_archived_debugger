1778959a36c4de2a9c4294e4793fe8f09fcc8d24
azban <me@azban.net>
2019-02-25 16:01:58 -0800

    web: add block import thread

--- block-explorer-api/src/near/block_explorer_api/web/run.py
+++ block-explorer-api/src/near/block_explorer_api/web/run.py
@@ -1,3 +1,8 @@
+import sys
+import threading
+import time
+
+from near.block_explorer_api import client
 from near.block_explorer_api.service import service
 from near.block_explorer_api.web.app import create_app
 
@@ -10,4 +15,21 @@ if __name__ == '__main__':
         with service.db.transaction_context():
             service.db.create_all()
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+    class BlockImportThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while True:
+                try:
+                    with app.app_context():
+                        client.import_beacon_blocks()
+                except Exception as e:
+                    print(e, file=sys.stderr)
+
+                time.sleep(10)
+
+
+    block_import_thread = BlockImportThread()
+    block_import_thread.start()
+
+    app.run(host='0.0.0.0', port=5000)
