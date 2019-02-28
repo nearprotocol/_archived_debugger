ead58653956133622a29b146c32129c78ffb5dae
azban <me@azban.net>
2019-02-27 17:15:44 -0800

    web: add gunicorn runner

--- api/src/near/debugger_api/web/run.py
+++ api/src/near/debugger_api/web/run.py
@@ -10,37 +10,37 @@ import time
 from near.debugger_api.api import DebuggerApi
 from near.debugger_api.web.app import create_app
 
+db_uri = os.environ.get('NEAR_DEBUGGER_DB_URI', 'sqlite:////tmp/debugger-api.db')
+application, db_session_getter, db_session_setter = create_app()
+application.api = DebuggerApi(
+    db_uri=db_uri,
+    thread_local_db_session_getter=db_session_getter,
+    thread_local_db_session_setter=db_session_setter,
+)
+
+api = DebuggerApi(db_uri=db_uri)
+with api.db.transaction_context():
+    api.db.create_all()
+
+api.db.session.remove()
+
+
+class BlockImportThread(threading.Thread):
+    @classmethod
+    def run(cls):
+        while True:
+            try:
+                api.import_beacon_blocks()
+            except Exception as e:
+                print(e, file=sys.stderr)
+            finally:
+                api.db.session.remove()
+
+            time.sleep(10)
+
+
+block_import_thread = BlockImportThread()
+block_import_thread.start()
+
 if __name__ == '__main__':
-    db_uri = os.environ.get('NEAR_DEBUGGER_DB_URI', 'sqlite:////tmp/debugger-api.db')
-    app, db_session_getter, db_session_setter = create_app()
-    app.api = DebuggerApi(
-        db_uri=db_uri,
-        thread_local_db_session_getter=db_session_getter,
-        thread_local_db_session_setter=db_session_setter,
-    )
-
-    api = DebuggerApi(db_uri=db_uri)
-    with api.db.transaction_context():
-        api.db.create_all()
-
-    api.db.session.remove()
-
-
-    class BlockImportThread(threading.Thread):
-        @classmethod
-        def run(cls):
-            while True:
-                try:
-                    api.import_beacon_blocks()
-                except Exception as e:
-                    print(e, file=sys.stderr)
-                finally:
-                    api.db.session.remove()
-
-                time.sleep(10)
-
-
-    block_import_thread = BlockImportThread()
-    block_import_thread.start()
-
-    app.run(host='0.0.0.0', port=5000)
+    application.run(host='0.0.0.0', port=5000, debug=True)
