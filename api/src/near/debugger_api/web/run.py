fef7e082fc85fd4af1b54b7629c775c5f5569e20
azban <me@azban.net>
2019-03-21 14:48:30 -0700

    api: add env var option for server address

--- api/src/near/debugger_api/web/run.py
+++ api/src/near/debugger_api/web/run.py
@@ -12,14 +12,16 @@ from near.debugger_api.api import DebuggerApi
 from near.debugger_api.web.app import create_app
 
 db_uri = os.environ.get('NEAR_DEBUGGER_DB_URI', 'sqlite:////tmp/debugger-api.db')
+server_address = os.environ.get('NEAR_DEBUGGER_SERVER_ADDRESS', 'http://localhost')
 application, db_session_getter, db_session_setter = create_app()
 application.api = DebuggerApi(
+    server_address=server_address,
     db_uri=db_uri,
     thread_local_db_session_getter=db_session_getter,
     thread_local_db_session_setter=db_session_setter,
 )
 
-api = DebuggerApi(db_uri=db_uri)
+api = DebuggerApi(server_address=server_address, db_uri=db_uri)
 with api.db.transaction_context():
     api.db.create_all()
 
